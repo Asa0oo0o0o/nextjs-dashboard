e001c3e9aaaaa3828af533a451c90b04edcbcbfa
Asa0oo0o0o <lipeirongasa@outlook.com>
2025-05-21 23:29:41 +0800

    feat: improve invoice edit-form accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,13 +70,25 @@ export async function createInvoices(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to update Invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -87,6 +99,9 @@ export async function updateInvoice(id: string, formData: FormData) {
     `;
   } catch (error) {
     console.log(error);
+    return {
+      message: "Database Error: Failed to update Invoice.",
+    };
   }
 
   revalidatePath("/dashboard/invoices");
